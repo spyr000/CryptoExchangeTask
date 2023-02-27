9600464dfa59d3fb091806952d3cb1609574fad5
Alexander <mr.zeddd123@gmail.com>
2023-02-27 18:04:51 +0300

    added UserDetailsImpl.java class

--- src/main/java/com/company/cryptoexchangetask/security/UserDetailsImpl.java
+++ src/main/java/com/company/cryptoexchangetask/security/UserDetailsImpl.java
@@ -1,49 +1,53 @@
 package com.company.cryptoexchangetask.security;
 
+import com.company.cryptoexchangetask.entities.user.User;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
-@RequiredArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserDetailsImpl implements UserDetails {
-
+    private User user;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
