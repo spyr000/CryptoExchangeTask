02fd8bc62897e6e7f99e580358b66a51e8008727
Alexander <mr.zeddd123@gmail.com>
2023-03-26 22:00:45 +0300

    s

--- src/main/java/com/company/cryptoexchangetask/controller/security/AuthenticationController.java
+++ src/main/java/com/company/cryptoexchangetask/controller/security/AuthenticationController.java
@@ -6,6 +6,7 @@ import com.company.cryptoexchangetask.dto.security.AuthenticationResponse;
 import com.company.cryptoexchangetask.dto.security.RegistrationResponse;
 import com.company.cryptoexchangetask.service.security.AuthenticationService;
 import com.company.cryptoexchangetask.service.security.impl.AuthenticationServiceImpl;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
     private final AuthenticationService service;
 
+    @Transactional(rollbackOn = Exception.class)
     @PostMapping("/register")
     public ResponseEntity<RegistrationResponse> register(
             @RequestBody RegisterRequest request
